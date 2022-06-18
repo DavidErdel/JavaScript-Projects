73c2b79d4eb3f6c533e958025bd5d883d48cfe01
DavidErdel <daviderdel@gmail.com>
2022-06-18 13:44:33 -0500

    First javascript assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+document.write("Hello, world! "); //Writes "Hello world!" in the webpage
+
+var A = "David said, \"This is David's string!\" "; //Uses the "\" to display quotation marks
+
+document.write(A) //Writes a variable
+
+document.write("\"Be who are and say what you feel" //Uses the plus sign to concatenate multiple lines
++" because those who mind don\'t matter and those who matter don\'t mind.\""
++" -Dr. Seuss"); 
+
+var B = "David said," //Assigns a variable to a concatenated set of strings
++"\"This is David's string!\"";
+
+document.write(B) //Writes the previous line on the webpage by using the variable
+
+var Family = "The Sierras", Dad = "Manuel Sr.", Mom = "Rosa Maria", //Assigns multiple variables
+ Daughter = "Ana", Son = "Manuel Jr.";
+
+ document.write(Son) //Writes one of the mutiple variables assigned
+
+ document.write(3+3) //Writes an expression
+
+ window.alert("This is a window alert!"); //Creates a window alert
