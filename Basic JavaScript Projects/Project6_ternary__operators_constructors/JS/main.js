897da5e5b0f04e2c168434fb187e4c530543ff98
DavidErdel <daviderdel@gmail.com>
2022-06-21 14:43:16 -0500

    PROJECT6_TERNARY_OPERATORS_CONSTRUCTORS

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,18 +1,18 @@
-function Ride_Function() {
+function Ride_Function() { //Uses ternary operation
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
-function Vote_Function() {
+function Vote_Function() { //Uses ternary operation
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
     Can_vote = (Age < 18) ? "You are too young":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
-function Vehicle(Make, Model, Year, Color) {
+function Vehicle(Make, Model, Year, Color) { //A constructor that uses "new" and "this"
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
@@ -27,7 +27,7 @@ function myFunction() {
     " manufactured in " + Erik.Vehicle_Year;
 }
 
-function Country(Continent, Capital, Size) {
+function Country(Continent, Capital, Size) { //A constructor that uses "new" and "this" plus a function to display the constructor in the HTML element
     this.Contry_Continent = Continent;
     this.Country_Capital = Capital;
     this.Country_Size = Size;
@@ -41,10 +41,10 @@ function Country(Continent, Capital, Size) {
       ". It is " + Colombia.Country_Size + " squares miles in size.";
   }
 
-var x = null
+var x = null //A variable assigned to the value of a reserved word
 document.write(x);
 
-function count_Function() {
+function count_Function() { //A nested function
     document.getElementById("Nested_Function").innerHTML = Count();
         function Count() {
             var Starting_point = 10;
