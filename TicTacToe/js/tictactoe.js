576ac7bb265235b2298b2adedd0d56d93b5dd1ac
DavidErdel <daviderdel@gmail.com>
2022-06-24 11:34:08 -0500

    Tic Tac Toe Take Two

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -90,7 +90,7 @@ function checkWinConditions() {
     //O 3, 4, 5 condition.
     else if (arrayIncludes('3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304) }
     //O 6, 7, 8 condition.
-    else if (arrayIncludes('0O', '3O', '8O')) {drawWinLine(50, 508, 558, 508) }
+    else if (arrayIncludes('6O', '7O', '8O')) {drawWinLine(50, 508, 558, 508) }
     //O 0, 3, 6 condition.
     else if (arrayIncludes('0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558) }
     //O 1, 4, 7 condition.
